b84209a6dab753d55ec5c1c7b63554d9668b8e42
Bernat Escarrà <bernat.escarra@gmail.com>
2022-08-21 22:29:13 +0200

    Update VoteList component

--- src/components/vote/VoteList.js
+++ src/components/vote/VoteList.js
@@ -1,13 +1,38 @@
+import { useState } from "react"
+import Swal from "sweetalert2"
+import VoteForm from "../containers/VoteForm"
+
 export default function VoteList(props) {
-	return (
-		<div className="bg-white u-round-sm u-shadow-lg px-2 py-3 grid-c-4-md">
-        <h4 className="u-text-center">What's your favourite language?</h4>
-        <div className="text-lg u-text-center mx-auto u-flex u-flex-column max-w-90p u-gap-2 py-3">
-          <input type="text" placeholder="Option 1" className="u-center" style={{ maxWidth: "200px" }} />
-          <input type="text" placeholder="Option 2" className="u-center" style={{ maxWidth: "200px" }} />
-          <input type="text" placeholder="Option 3" className="u-center" style={{ maxWidth: "200px" }} />
-        </div>
-        <button className="bg-green-600 text-white u-center">Submit</button>
+  const { question, nextStep, saveAnswer } = props
+  const [answer, setAnswer] = useState(null)
+
+  const handleFormChange = (e) => {
+    setAnswer(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (question.isCompulsory && !answer) {
+      Swal.fire('Answering to this question is compulsory')
+      return
+    }
+    if (answer) {
+      saveAnswer({ 'list': answer })
+      setAnswer(null)
+    } else if (!answer) {
+      nextStep()
+    }
+  }
+
+  return (
+    <VoteForm onSubmit={handleSubmit}>
+      <h4 className="u-text-center">{question.title}<font className="text-red-700">{question.isCompulsory ? '*' : ''}</font></h4>
+      <div className="text-lg u-text-center mx-auto u-flex u-flex-column max-w-90p u-gap-2 py-3">
+        {[...Array(question.listNumber)].map((option, i) => {
+          return <input key={i} type="text" placeholder={`Answer ${i + 1}`} className="u-center" style={{ maxWidth: "200px" }} onChange={e => handleFormChange(e)} />
+        })}
       </div>
-	)
+      <button type="submit" className="bg-teal-600 text-white u-center">Submit answer</button>
+    </VoteForm>
+  )
 }
