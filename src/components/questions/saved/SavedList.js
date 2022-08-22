e30bd4ee7dd52a8870fac95788433c35cf7786f8
Bernat Escarrà <bernat.escarra@gmail.com>
2022-08-22 16:11:03 +0200

    Update SavedList component

--- src/components/questions/saved/SavedList.js
+++ src/components/questions/saved/SavedList.js
@@ -2,26 +2,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faToggleOff, faGrip, faPen } from '@fortawesome/free-solid-svg-icons'
 
 export default function SavedList(props) {
+    const { index, CRUD: { updateQuestion }, question: { title, isCompulsory, listNumber } } = props
+
+    const handleEdit = (e) => {
+        e.preventDefault()
+        updateQuestion(index, 'isSaved', false)
+    }
+
     return (
         <div className="bg-white u-round-sm u-shadow-lg px-3 pt-2 pb-1">
-            {/* Question header */}
             <div className="u-flex u-flex-column u-justify-center u-relative pb-1">
                 <FontAwesomeIcon className="text-gray-600 grip top-grip" icon={faGrip} />
                 <div className="u-absolute-md u-left-0 u-right-0">
                     <div className="u-flex u-flex-row-md u-justify-space-between-md u-items-center u-flex-column">
                         <em className="font-bold">List question</em>
-                        <div className="u-flex u-gap-1 text-xs text-gray-800">Not compulsory <FontAwesomeIcon className="text-xl text-gray-800" style={{ cursor: "pointer" }} icon={faToggleOff} /></div>
+                        <div className="u-flex u-gap-1 text-xs text-gray-800 hidden">Not compulsory <FontAwesomeIcon className="text-xl text-gray-800" style={{ cursor: "pointer" }} icon={faToggleOff} /></div>
                     </div>
                 </div>
             </div>
-            <div className="u-text-break font-alt font-bold py-1 text-yellow-400 u-text-center" style={{ lineHeight: "1.2rem" }}>This is a list question title example, what do you think? <FontAwesomeIcon className="text-gray-600 text-sm" icon={faPen} /></div>
-            {/* Question header */}
-            <form className="u-flex u-flex-column u-items-center">
-                <label className="mb-0 text-sm">Set how many options the user should enter</label>
-                <input type="number" placeholder="3" style={{ maxWidth: "75px" }} />
-            </form>
-            <button className="text-white bg-indigo-900 btn--sm u-pull-right mr-1">Save</button>
-            {/* Question body */}
+            <div className="p-1 px-10-md u-text-center mt-3">
+                <div className="mx-24-md p-4 u-round-sm u-shadow-md" style={{ border: "solid 1px lightgrey" }}>
+                    <h4>{title}<font className="text-red-700">{isCompulsory ? '*' : ''}</font></h4>
+                    <div className="text-lg u-text-center mx-auto u-flex u-flex-column max-w-90p u-gap-2 py-2">
+                        {[...Array(Number(listNumber))].map((field, i) => {
+                            return <input disabled key={i} type="text" placeholder={`Answer ${i + 1}`} className="u-center input--xs" style={{ maxWidth: "200px", cursor: "default" }} />
+                        })}
+                    </div>
+                </div>
+            </div>
+            <button onClick={handleEdit} className="bg-teal-600 text-white btn--sm u-pull-right mr-1">Edit <FontAwesomeIcon icon={faPen} /></button>
         </div>
     )
 }
