d6db8e61a4723e74c3e814fbcf1b8a7e62b8487a
Bernat Escarrà <bernat.escarra@gmail.com>
2022-08-22 15:06:34 +0200

    Update VoteList.js

--- src/components/vote/VoteList.js
+++ src/components/vote/VoteList.js
@@ -1,27 +1,30 @@
-import { useState } from "react"
 import Swal from "sweetalert2"
 import VoteForm from "../containers/VoteForm"
 
 export default function VoteList(props) {
   const { question, nextStep, saveAnswer } = props
-  const [answer, setAnswer] = useState(null)
-
-  const handleFormChange = (e) => {
-    setAnswer(e.target.value)
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (question.isCompulsory && !answer) {
-      Swal.fire('Answering to this question is compulsory')
+
+    const answersArray = []
+    for (let i = 0; i < e.target.length - 1; i++) {
+      answersArray.push((e.target[i]).value)
+    }
+
+    const numberOfAnswers = answersArray.reduce((previousValue, currentValue) => {
+      if (currentValue !== '') { return previousValue + 1 }
+      else { return previousValue }
+    }, 0)
+
+    if (question.isCompulsory && (numberOfAnswers < question.listNumber)) {
+      Swal.fire('You must provide all the answers')
       return
     }
-    if (answer) {
-      saveAnswer({ 'list': answer })
-      setAnswer(null)
-    } else if (!answer) {
-      nextStep()
-    }
+
+    console.log(answersArray)
+    saveAnswer({ 'list': answersArray })
+    nextStep()
   }
 
   return (
@@ -29,7 +32,7 @@ export default function VoteList(props) {
       <h4 className="u-text-center">{question.title}<font className="text-red-700">{question.isCompulsory ? '*' : ''}</font></h4>
       <div className="text-lg u-text-center mx-auto u-flex u-flex-column max-w-90p u-gap-2 py-3">
         {[...Array(question.listNumber)].map((option, i) => {
-          return <input key={i} type="text" placeholder={`Answer ${i + 1}`} className="u-center" style={{ maxWidth: "200px" }} onChange={e => handleFormChange(e)} />
+          return <input key={i} type="text" placeholder={`Answer ${i + 1}`} className="u-center" style={{ maxWidth: "200px" }} />
         })}
       </div>
       <button type="submit" className="bg-teal-600 text-white u-center">Submit answer</button>
