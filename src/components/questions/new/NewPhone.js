ce18f22d7cc872587a6ec403995cfe8aaaaaddc3
Bernat Escarrà <bernat.escarra@gmail.com>
2022-08-22 16:23:15 +0200

    Update phone question components

--- src/components/questions/new/NewPhone.js
+++ src/components/questions/new/NewPhone.js
@@ -72,7 +72,7 @@ export default function NewPhone(props) {
           <font className="dotted">{title ? `${title}${isCompulsory ? '*' : ''}` : <>Edit your title here!{isCompulsory && '*'}</>}</font> {<FontAwesomeIcon className={showPen ? "text-gray-600 text-sm" : "text-gray-600 text-sm hidden"} icon={faPen} />}
         </div>
       </div>
-      <input disabled type="tel" placeholder="+66 666666666" className="u-center" style={{ maxWidth: "180px" }} />
+      <input disabled type="tel" placeholder="+66 666666666" className="u-center" style={{ maxWidth: "180px", cursor: "default" }} />
       <form onSubmit={saveQuestion}>
         <button type="submit" className="text-white bg-teal-600 btn--sm u-pull-right mr-1">Save</button>
       </form>
--- src/components/questions/saved/SavedPhone.js
+++ src/components/questions/saved/SavedPhone.js
@@ -24,7 +24,7 @@ export default function SavedPhone(props) {
       <div className="p-1 px-10-md u-text-center mt-3">
         <div className="mx-24-md p-4 u-round-sm u-shadow-md" style={{ border: "solid 1px lightgrey" }}>
         <h4>{title}<font className="text-red-700">{isCompulsory ? '*' : ''}</font></h4>
-        <input disabled type="tel" placeholder="+66 666666666" className="u-center" style={{ maxWidth: "180px" }} />
+        <input disabled type="tel" placeholder="+66 666666666" className="u-center" style={{ maxWidth: "180px", cursor: "default" }} />
         </div>
       </div>
       <button onClick={handleEdit} className="bg-teal-600 text-white btn--sm u-pull-right mr-1">Edit <FontAwesomeIcon icon={faPen} /></button>
