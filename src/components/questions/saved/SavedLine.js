a2b8beff10d92e934273586a300e59063bcf91ce
Bernat Escarrà <bernat.escarra@gmail.com>
2022-08-22 16:14:49 +0200

    Update SavedLine component

--- src/components/questions/saved/SavedLine.js
+++ src/components/questions/saved/SavedLine.js
@@ -3,7 +3,7 @@ import { faToggleOff, faGrip, faPen } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function SavedLine(props) {
-  const {index, CRUD: {updateQuestion}, question: {title, isCompulsory}} = props
+  const { index, CRUD: { updateQuestion }, question: { title, isCompulsory } } = props
 
   const handleEdit = (e) => {
     e.preventDefault()
@@ -16,15 +16,15 @@ export default function SavedLine(props) {
         <FontAwesomeIcon className="text-gray-600 grip top-grip" icon={faGrip} />
         <div className="u-absolute-md u-left-0 u-right-0">
           <div className="u-flex u-flex-row-md u-justify-space-between-md u-items-center u-flex-column">
-            <em className="font-bold">Open question (paragraph)</em>
+            <em className="font-bold">Open question (single line)</em>
             <div className="u-flex u-gap-1 text-xs text-gray-800 hidden">Not compulsory <FontAwesomeIcon className="text-xl text-gray-800" style={{ cursor: "pointer" }} icon={faToggleOff} /></div>
           </div>
         </div>
       </div>
       <div className="p-1 px-10-md u-text-center mt-3">
         <div className="mx-24-md p-4 u-round-sm u-shadow-md" style={{ border: "solid 1px lightgrey" }}>
-        <h4>{title}<font className="text-red-700">{isCompulsory ? '*' : ''}</font></h4>
-          <textarea disabled style={{ cursor: "default", resize: "none" }} className="max-w-50p u-center" placeholder="Textarea"></textarea>
+          <h4>{title}<font className="text-red-700">{isCompulsory ? '*' : ''}</font></h4>
+          <input disabled style={{ cursor: "default", resize: "none" }} className="u-center input--sm" placeholder="The user will be able to provide a single line as an answer"></input>
         </div>
       </div>
       <button onClick={handleEdit} className="bg-teal-600 text-white btn--sm u-pull-right mr-1">Edit <FontAwesomeIcon icon={faPen} /></button>
